08db88f46e2f373768a2f984a2ef138d309c9049
huudat2910 <125776010+huudat2910@users.noreply.github.com>
2023-03-12 02:13:09 +0700

    Update thamsodinhdang_Sep.py
    add source code

---
+++ thamsodinhdang_Sep.py
@@ -0,0 +1,6 @@
+#Nhap gia tri {Ten cua ban}
+ten = input()
+
+#Xuat cau voi dinh dang theo yeu cau de bai
+#Tham so sep='--' giup ngan cach cac tu boi '--'
+print('Xin', 'chao!', 'Toi', 'ten', 'la', ten, sep='--')
