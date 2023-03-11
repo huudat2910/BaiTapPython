5c82f9eabe13fd9b840431f94aee78d35cd0627c
huudat2910 <datdinh2910@gmail.com>
2023-03-12 02:00:26 +0700

    add input_except file

---
+++ input_except.py
@@ -0,0 +1,8 @@
+try: #khoi lenh co the phat sinh loi
+    So1 = int(input('nhap so nguyen thu nhat: '))
+    So2 = int(input('nhap so nguyen thu hai: '))   
+    
+
+    print("Tong hai so nguyen la : ", So1+So2)
+except: # khoi lenh duoc thuc thi khi loi xay ra
+    print("Vui long nhap so")
